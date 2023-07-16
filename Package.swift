c723a30922bf8dd165163e0a50c4bb24e57391ff
clinchcode <110381715+clinchcode@users.noreply.github.com>
2023-07-16 12:11:40 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+import PackageDescription
+let package = Package(
+    name: "ClotGuideSwift",
+    platforms: [.macOS(.v11), .iOS(.v13)],
+    products: [
+        .executable(name: "ClotGuideSwift", targets: ["ClotGuideSwift"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/TokamakUI/Tokamak", from: "0.11.0")
+    ],
+    targets: [
+        .executableTarget(
+            name: "ClotGuideSwift",
+            dependencies: [
+                "ClotGuideSwiftLibrary",
+                .product(name: "TokamakShim", package: "Tokamak")
+            ]),
+        .target(
+            name: "ClotGuideSwiftLibrary",
+            dependencies: []),
+        .testTarget(
+            name: "ClotGuideSwiftLibraryTests",
+            dependencies: ["ClotGuideSwiftLibrary"]),
+    ]
+)
